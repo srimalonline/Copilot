220e7aebb89a93f285034fe07e5724c63abe1990
Srimal Fernando <67434227+srimalonline@users.noreply.github.com>
2023-02-02 23:46:27 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,1 @@
-//create an animation instance
-var myAnimation = bodymovin.loadAnimation({
-    container: document.getElementById('bm'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'data.json'
-})
+
